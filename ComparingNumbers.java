88f526314620a32d56ef1194d3c7196a0a5149b5
Mohsen-Asgari-1993 <47674992+Mohsen-Asgari-1993@users.noreply.github.com>
2019-03-07 09:33:20 +0330

    Add files via upload

---
+++ ComparingNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ComparingNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[3];
+        System.out.println(" please enter your numbers :");
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = input.nextInt();
+        }
+        System.out.println(max(numbers));
+    }
+
+    static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
